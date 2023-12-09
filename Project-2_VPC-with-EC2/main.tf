945f2f903bb86a07748de6af4a59796ba5f36f6a
Subho4563 <78839397+Subho4563@users.noreply.github.com>
2023-12-10 00:33:20 +0530

    Update main.tf

--- Project-2_VPC-with-EC2/main.tf
+++ Project-2_VPC-with-EC2/main.tf
@@ -1,1 +1,23 @@
+resource "aws_vpc" "my-vpc" {
+    cidr_block = var.cidr
+}
 
+resource "aws_subnet" "subnet1" {
+    vpc_id = aws_vpc.my-vpc.id
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = "true"
+  }
+
+resource "aws_security_group" "sg1" {
+    name = "security"
+    vpc_id = aws_vpc.my-vpc.id
+  }
+
+resource "aws_instance" "subho" {
+    ami = "ami-0fc5d935ebf8bc3bc"
+    instance_type = "t2.micro"
+    key_name = "deep.login"
+    subnet_id = aws_subnet.subnet1.id
+    vpc_security_group_ids = [aws_security_group.sg1.id]
+}
