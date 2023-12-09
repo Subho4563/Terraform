652dec1b92dfb250b0d38631e7e980c28b20bea2
Subho4563 <78839397+Subho4563@users.noreply.github.com>
2023-12-09 23:40:16 +0530

    Update main.tf

--- Project-1_EC2_creation/main.tf
+++ Project-1_EC2_creation/main.tf
@@ -1,1 +1,10 @@
+provider "aws" {
+    region = "us-east-1"
+}
 
+resource "aws_instance" "instance1" {
+    ami = "ami-0fc5d935ebf8bc3bc"
+    instance_type = "t2.micro"
+    key_name = "deep.login"
+    
+    }  
